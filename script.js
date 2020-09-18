a9418bac2d8f2592afcf15faa7dcd97c9ab5ab05
elaa-git <69598207+elaa-git@users.noreply.github.com>
2020-09-18 22:50:32 +0200

    Create script.js
    Programming Foundations - Answers

---
+++ script.js
@@ -0,0 +1,37 @@
+// question 1
+
+var firstVar;
+
+// question 2
+
+var name = "eli";
+
+// question 3
+
+var value = 100;
+
+// question 4
+
+var division = 20 / 5;
+
+// question 5
+
+typeof (frog);
+
+// question 6
+
+var orderHasShipped = false;
+
+// question 7
+
+if(orderHasShipped === true) {
+    console.log("True");
+}
+    else {console.log("False");
+}
+
+// question 8
+
+for(var count = 0; count <= 9; count++) {
+    console.log(count);
+}
